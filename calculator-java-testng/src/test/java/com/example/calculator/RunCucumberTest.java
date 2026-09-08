f5e6e0c32d040c9f7a6bd624fcced8dd50e326b0
M.P. Korstanje <rien.korstanje@gmail.com>
2026-09-08 16:33:00 +0200

    testng: Make RunCucumberTest public

--- calculator-java-testng/src/test/java/com/example/calculator/RunCucumberTest.java
+++ calculator-java-testng/src/test/java/com/example/calculator/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(plugin = { "html:target/results.html", "message:target/results.ndjson" })
-class RunCucumberTest extends AbstractTestNGCucumberTests {
+public class RunCucumberTest extends AbstractTestNGCucumberTests {
 
     @DataProvider(parallel = true)
     @Override
